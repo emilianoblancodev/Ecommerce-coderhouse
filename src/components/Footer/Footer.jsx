2c1dfca0c9c8655b76f05111d2fcdf2305ac26bc
= <=>
2022-03-10 11:39:14 -0300

    Agrego iconos de redes sociales al footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
 import './Footer.css'
+import facebook from '../../img/facebook.png'
+import twitter from '../../img/twitter.png'
+import youtube from '../../img/youtube.png'
+import instagram from '../../img/instagram.png'
+import whatsapp from '../../img/whatsapp.png'
+
+
 
 const Footer = () => {
   return (
     <>
         <Container fluid className="footer">
             <Row>
-                <Col className="text-center">
-                <h1>footer</h1>
+                <Col className="text-center mt-5">
+                <h4>Podes seguirnos en nuestras redes sociales</h4>
+                <div className="redes">
+                    <img src={facebook}/>
+                    <img src={instagram}/>
+                    <img src={twitter}/>
+                    <img src={youtube}/>
+                
+                </div>
+                
                 </Col>
-                <Col className="text-center">
-                <h1>footer</h1>
+                <Col className="text-center mt-5">
+                <h4>Contactanos para cualquier consulta</h4>
+                <div className="redes">
+                    <img src={whatsapp} />
+                </div>
+                
                 </Col>
             </Row>
             
