c260a2552478d2a94ab83f4e985a326bb01f66b8
= <=>
2022-03-10 17:43:51 -0300

    Agrego texto para la pagina "sobre nosotros"

--- src/components/AboutMePage/AboutMePage.jsx
+++ src/components/AboutMePage/AboutMePage.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import Carrousel from '../Carrousel/Carrousel'
+import {Container, Row} from 'react-bootstrap'
 
 const AboutMePage = () => {
   return (
     <>
-      <h1>AboutMePage - Aca pondre un texto descriptivo de la empresa</h1>
+    <Container>
+
+      <h1 className="text-center">¿Quiénes somos?</h1>
+      <p>
+        Nuestra historia comienza allá por 1970 con la inauguración de nuestra primera tienda física, El rey de las zapatillas. Con el correr de los años nos fuimos expandiendo cada vez más a través de la apertura de nuevas sucursales, hasta que en 1986 abrimos el primer local integral de deportes. Desde ese momento, todo fue crecimiento: realizamos alianzas estratégicas con otras marcas para ampliar nuestras redes e inauguramos un nuevo concepto de tienda deportiva.
+      </p>
+
+      <p>
+          En movimiento constante y fusionando las últimas tendencias de la moda urbana y el deporte, en 2016 lanzamos Moov, una nueva propuesta orientada al público joven que incorpora nuevas marcas y diseños de vanguardia. Nos adaptamos a tiempos de cambios, a las nuevas generaciones, y por eso te traemos los más exclusivos productos de las mejores marcas para que crees tu propio estilo.
+      </p>
+      <p>
+        Misión: 
+        Vivimos entrenando para ganar y conquistar a los consumidores ofreciendo una experiencia premium. Hablamos a través del deporte y de la moda con el objetivo de acompañarlos en cada momento de sus vidas para que puedan competir, entrenar o lucir su estilo personal, transmitiendo inspiración e innovación.
+      </p>
+      <p>
+        Visión:
+        Ser el retailer líder en ventas y en servicio al cliente, siendo siempre cercano al consumidor.
+
+        Nuestros Valores: 
+        -Cliente siempre primero
+        -Compromiso
+        -Pasión
+        -Hacer lo correcto
+        -Más que un equipo
+        -Innovación
+        -Comunicación
+      </p>
+    </Container>
+      
     </>
     
   )
