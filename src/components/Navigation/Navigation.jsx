978cb06098f111701ea2dd3f210658ea4473fd4c
= <=>
2022-03-10 10:50:00 -0300

    Cambio icono del carrito

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -13,7 +13,7 @@ import AboutMePage from '../AboutMePage/AboutMePage';
 import ContactPage from '../ContactPage/ContactPage';
 import ShopPage from '../ShopPage/ShopPage';
 import Intro from '../Intro/Intro'
-import carrito from '../../img/carrito2.png'
+import carrito from '../../img/cart_white_24dp.png'
 import './Navigation.css'
 
 const Navigation = () => {
