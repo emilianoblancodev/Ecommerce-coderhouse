1ef0c096e3402a0a0f57f24dfa72d94249ca46b7
= <=>
2022-03-17 20:21:26 -0300

    Creo tabla para el cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,7 +9,24 @@ const Cart = () => {
   return (
     <>
     <h2>Carrito</h2>
-      {cartList.map(item => <li>{item.imagen}{item.titulo}{item.precio}</li>)}
+      {/* {cartList.map(item => <li>{item.imagen}{item.titulo}{item.precio}</li>)} */}
+
+      {cartList.map(item => <table>
+                              <tr>
+                                <td>
+                                  {item.imagen}
+                                </td>
+                                <td>
+                                  {item.titulo}
+                                </td>
+                                <td>
+                                  {item.precio}
+                                </td>
+                              </tr>
+                            </table>
+      
+      )}
+
       <button onClick={clearCart}>Vaciar Carrito</button>
     </>
     
